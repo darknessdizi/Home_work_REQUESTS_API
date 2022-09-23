7979d98a8ca240e0b9b318c5b3fc16f958e78dcd
Remezov Dmitry <darknessdizi@gmail.com>
2022-09-24 03:12:07 +0800

    change task_2 and add comments in task_2, add list_file

--- task_2_request_put_save_file.py
+++ task_2_request_put_save_file.py
@@ -5,15 +5,24 @@ import requests
 class YaUploader:
 
     def __init__(self, token: str):
+
+        '''Конструктор класса'''
+
         self.token = token
 
     def get_headers(self):
+
+        '''Заголовки для авторизации на яндекс диск'''
+
         return {
             'Content-Type': 'application/json',
             'Authorization': f'OAuth {self.token}'
         }
 
     def upload(self, file_path: str):
+
+        '''Загружаем файл на яндекс диск'''
+
         url = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
         path = file_path.split('/')
         if len(path) > 1:
@@ -25,7 +34,10 @@ class YaUploader:
         with open(file_path, 'rb') as file:
             response = requests.put(response['href'], files={'file': file}, headers=headers, params=parameters)
 
-    def get_file(self, url):
+    def _get_file(self, url):
+
+        '''Тестовая функция'''
+
         headers = self.get_headers()
         response = requests.get(url, headers=headers).json()
         for files in response['items']:
@@ -33,12 +45,18 @@ class YaUploader:
                 print(y)
 
     def create_folder(self, list_folders):
+
+        '''Создаем папку на яндекс диск'''
+
         url = 'https://cloud-api.yandex.net:443/v1/disk/resources' 
         headers = self.get_headers()
         parameters = {'path': list_folders, 'overwrite': 'false'}
         response = requests.put(url, headers=headers, params=parameters)
 
 def open_file_token(path, encoding):
+
+    '''Скачиваем token из текстового файла'''
+
     with open(path, encoding=encoding) as file:
         reader = file.readline().rstrip('\n')
     return reader
@@ -46,9 +64,13 @@ def open_file_token(path, encoding):
 
 if __name__ == '__main__':
 
-    path_to_file = 'list_file/test1.txt'
+    path_to_file = os.listdir('list_file')
+    for index in range(len(path_to_file)):
+        path_to_file[index] = 'list_file/' + path_to_file[index]
+    # path_to_file = 'list_file/test1.txt'
     token = open_file_token('token.txt', 'utf-8')
     uploader = YaUploader(token)
-    # file = uploader.get_file('https://cloud-api.yandex.net:443/v1/disk/resources/files')
-    result = uploader.upload(path_to_file)
+    # # file = uploader._get_file('https://cloud-api.yandex.net:443/v1/disk/resources/files')
+    for path in path_to_file:
+        result = uploader.upload(path)
    
